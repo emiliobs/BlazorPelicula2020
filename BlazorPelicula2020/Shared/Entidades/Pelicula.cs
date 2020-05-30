a3078bdc6009b5095f0e147ed8911e9ee093302c
Emilio Barrea <barrera_emilio@yahoo.es>
2020-05-30 22:46:47 +0100

    Utilizando Clase

---
+++ BlazorPelicula2020/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPelicula2020.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+    }
+}
