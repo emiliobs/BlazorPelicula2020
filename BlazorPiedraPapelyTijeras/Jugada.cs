5f7cc417264309e3c44c1858d46aa7c52eea7b48
Emilio Barrea <barrera_emilio@yahoo.es>
2020-06-13 15:30:10 +0100

    Blazor app Piedra, Papel y Tijera already

---
+++ BlazorPiedraPapelyTijeras/Jugada.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPiedraPapelyTijeras
+{
+     public enum OPtionPPT
+    {
+        Piedra, papel, Tijera
+    }
+
+    public enum EstatusJuego
+    {
+        Victoria, Derrota, Empate
+    }
+    public class Jugada
+    {
+        public OPtionPPT OPtionPPT { get; set; }
+        public OPtionPPT VenceA { get; set; }
+        public OPtionPPT PierdeContra { get; set; }
+        public string Imagen { get; set; }
+
+       public EstatusJuego  JugarContra(Jugada jugada)
+        {
+            if (OPtionPPT == jugada.OPtionPPT)
+            {
+                return EstatusJuego.Empate;
+            }
+
+            if (VenceA == jugada.OPtionPPT)
+            {
+                return EstatusJuego.Victoria;
+            }
+
+            return EstatusJuego.Derrota;
+        }
+    }
+
+
+}
